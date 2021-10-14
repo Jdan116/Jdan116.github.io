945e137480fac76e2d90e6ea82e31a242e9e2e0b
Jdany <jifardaniel@gmail.com>
2021-10-13 23:00:40 -0500

    minor improvement

--- w3d3/maze.js
+++ w3d3/maze.js
@@ -4,41 +4,53 @@ $(function () {
 
     $("#start").click(function (e) {
 
+        // flag to track if the game is finished or error occurred
         isFinished = false;
         isErrorOccurred = false;
 
+        //reset div color on start button clicked
         $('#status_msg').css("visibility", "hidden");
         $(".boundary").css("background-color", "#eeeeee");
-        $("div.boundary").mouseenter(function () {
 
-            console.log("mouseenter: " + isFinished)
+        //if boundary crossed show error message and change the div color
+        $("div.boundary").mouseenter(function () {
             if (!isFinished) {
                 isErrorOccurred = true;
                 $(".boundary").css("background-color", "#f8c452");
-                $('#status_msg').css("visibility", "visible").text('Sorry! You have lost the game.').css({
+                $('#status_msg')
+                .text('Sorry! You have lost the game.')
+                .css({
+                    'visibility': 'visible',
                     'text-align': 'center',
                     'color': 'red'
                 });
             }
         });
 
+
+        //if the curse arrive to end without crossing any forbidden boundry, show success message
         $("#end").mouseenter(function () {
-            console.log("isErrorOccurred: " + isErrorOccurred)
             if (!isErrorOccurred) {
                 isFinished = true;
                 isErrorOccurred = false;
-                $('#status_msg').css("visibility", "visible").text('Congratulations! You have won the game. Click S to start the game again.').css({
+                $('#status_msg')
+                .text('Congratulations! You have won the game. Click S to start the game again.')
+                .css({
+                    'visibility': 'visible',
                     'text-align': 'center',
                     'color': 'green'
                 });
             }
         });
 
+        // check if user try to leave the game area after they start the game
         $("#maze").mouseleave(function () {
-            console.log("mouseleave: " + isFinished)
             if (!isFinished) {
                 $(".boundary").css("background-color", "#f8c452");
-                $('#status_msg').css("visibility", "visible").text('Sorry! You have lost the game.').css({
+                $('#status_msg')
+                .text('Sorry! You have lost the game.')
+                .css({
+                    'visibility': 'visible',
                     'text-align': 'center',
                     'color': 'red'
                 });
